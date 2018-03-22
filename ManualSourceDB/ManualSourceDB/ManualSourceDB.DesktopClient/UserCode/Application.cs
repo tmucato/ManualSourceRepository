2a24a478a3218e2cd5d0dea16a724df20fbab61c
tmucato <tomasmucato@gmail.com>
2018-03-22 16:19:00 +0200

    Revert "Merge branch 'master' of https://github.com/tmucato/ManualSourceRepository"
    This reverts commit addb70048b725f3812b2507785c27a26aa338bc8, reversing
    changes made to e6a2800b2ff448342cd45b71abd01c80ccff0604.

---
+++ ManualSourceDB/ManualSourceDB/ManualSourceDB.DesktopClient/UserCode/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class Application
+    {
+        partial void mothersListDetail_CanRun(ref bool result)
+        {
+            // Set result to the desired field value
+
+        }
+    }
+}
---
+++ ManualSourceDB/ManualSourceDB/ManualSourceDB.Server/DataSources/dbmanualsourceData/child.lsml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class child
+    {
+        partial void dob_Validate(EntityValidationResultsBuilder results)
+        {
+
+            if (this.dob.HasValue && this.dob > DateTime.Today)
+            {
+                results.AddPropertyError("Data de Nascimento não pode ser no futuro.");
+            }
+            if (this.dob.HasValue && this.dob <= Convert.ToDateTime("01-01-1900"))
+            {
+                results.AddPropertyError("Data de Nascimento não pode ser menor que 01-01-1900.");
+            }
+
+        }
+    }
+}
